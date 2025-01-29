bb0726633c9e4efebf1b35a8a93102363e0b93f4
TOM-FERREIRA-commits <tomdeveloper95@gmail.com>
2025-01-29 01:01:04 -0300

    first commit

---
+++ src/main/java/com/EltonFerreira/bookstoremananger/BookstoremanangerApplication.java
@@ -0,0 +1,13 @@
+package com.EltonFerreira.bookstoremananger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EltonFerreira/bookstoremananger/BookstoremanangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EltonFerreira.bookstoremananger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
