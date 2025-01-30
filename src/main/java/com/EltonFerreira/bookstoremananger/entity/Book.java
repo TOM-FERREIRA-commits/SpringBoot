bff28bcaecfb8ccff0f0861fdc56c240764a82aa
TOM-FERREIRA-commits <tomdeveloper95@gmail.com>
2025-01-29 21:48:09 -0300

    Develop project entities

---
+++ src/main/java/com/EltonFerreira/bookstoremananger/entity/Book.java
@@ -0,0 +1,42 @@
+package com.EltonFerreira.bookstoremananger.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String PublisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public void  author() {
+        author.getName();
+    }
+}
